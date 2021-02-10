699edd575a5ca77a3ab39f9a9101347df77b8169
AlwinSunil <alv12alwin@gmail.com>
2021-02-10 15:20:53 +0530

    v1 - Bug Fixes

---
+++ app/js/script.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDZgGVIfp3dq-2vI7cW_Onv5cFth3HNqZU",
+    authDomain: "profile-e22c1.firebaseapp.com",
+    projectId: "profile-e22c1",
+    storageBucket: "profile-e22c1.appspot.com",
+    messagingSenderId: "335802871020",
+    appId: "1:335802871020:web:e05d98edbe46652fd909c9",
+    measurementId: "G-GQ6ZHHQGMT"
+};
+// var storage = firebase.storage("gs://profile-e22c1.appspot.com/");
+// var spaceRef = storageRef.child('github.svg');
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
