e7328b977131a4ac2617eeb7d9aa52c6592a7c3a
AlwinSunil <alv12alwin@gmail.com>
2021-02-22 19:31:52 +0530

    v1.1 - Changed layout and fixed bugs.

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,31 @@
+$(window).on("load",function(){
+  $(".loader-wrapper").fadeOut("slow");
+});
+
+
 var firebaseConfig = {
-    apiKey: "AIzaSyDZgGVIfp3dq-2vI7cW_Onv5cFth3HNqZU",
-    authDomain: "profile-e22c1.firebaseapp.com",
-    projectId: "profile-e22c1",
-    storageBucket: "profile-e22c1.appspot.com",
-    messagingSenderId: "335802871020",
-    appId: "1:335802871020:web:e05d98edbe46652fd909c9",
-    measurementId: "G-GQ6ZHHQGMT"
+  apiKey: "AIzaSyDZgGVIfp3dq-2vI7cW_Onv5cFth3HNqZU",
+  authDomain: "profile-e22c1.firebaseapp.com",
+  projectId: "profile-e22c1",
+  storageBucket: "profile-e22c1.appspot.com",
+  messagingSenderId: "335802871020",
+  appId: "1:335802871020:web:e05d98edbe46652fd909c9",
+  measurementId: "G-GQ6ZHHQGMT",
 };
-// var storage = firebase.storage("gs://profile-e22c1.appspot.com/");
-// var spaceRef = storageRef.child('github.svg');
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var perf = firebase.performance();
 firebase.analytics();
 
+document.addEventListener("DOMContentLoaded", function () {
+  heightDiv();
+});
+
+function heightDiv() {
+  var quoteHeight = document.getElementById("quote__size").offsetHeight;
+  var sectionHeight = document.getElementById("section").offsetHeight;
+
+  var profileAreaHeight = sectionHeight - quoteHeight;
+  document.getElementById("profile").style.height = profileAreaHeight + "px";
+}
