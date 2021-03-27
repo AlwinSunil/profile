b802d85f8b4b89e7d8256ba39e3dfb272a984ce7
AlwinSunil <alv12alwin@gmail.com>
2021-03-27 13:52:54 +0530

    v1.3 - Fixed Bugs.

--- app/js/script.js
+++ app/js/script.js
@@ -2,17 +2,3 @@ $(window).on("load", function () {
   AOS.init();
   $(".loader-wrapper").fadeOut("slow");
 });
-
-var firebaseConfig = {
-  apiKey: "AIzaSyDZgGVIfp3dq-2vI7cW_Onv5cFth3HNqZU",
-  authDomain: "profile-e22c1.firebaseapp.com",
-  projectId: "profile-e22c1",
-  storageBucket: "profile-e22c1.appspot.com",
-  messagingSenderId: "335802871020",
-  appId: "1:335802871020:web:e05d98edbe46652fd909c9",
-  measurementId: "G-GQ6ZHHQGMT"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
